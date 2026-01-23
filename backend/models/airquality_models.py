e0a093dfa0ea8186cf827389b31fc090f481c519
Parker <kavanap3@tcd.ie>
2026-01-23 10:35:28 +0000

    core models defined for project

---
+++ backend/models/airquality_models.py
@@ -0,0 +1,3 @@
+class AirQualityMetrics:
+    def __init__(self, status):
+        self.status = status  # status of air Quality "good", "mid", "bad"
---
+++ backend/models/population_models.py
@@ -0,0 +1,4 @@
+class PopulationMetrics:
+    def __init__(self, area_name, quantity):
+        self.area_name = area_name
+        self.quantity = quantity  # census population in this area
