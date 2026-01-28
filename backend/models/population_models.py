95968ef80ec384cd03dc9c24cec23c2c08b75613
omallech <personrnan@gmail.com>
2026-01-28 16:41:16 +0000

    adjusted population model to include latitude and logitude

--- backend/models/population_models.py
+++ backend/models/population_models.py
@@ -1,4 +1,6 @@
 class PopulationMetrics:
-    def __init__(self, area_name, quantity):
+    def __init__(self, area_name, latitude, longitude, quantity):
         self.area_name = area_name
+        self.latitude = latitude  # latitude coordinate of the area
+        self.longitude = longitude  # longitude coordinate of the area
         self.quantity = quantity  # census population in this area
