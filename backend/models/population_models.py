fc3e159d35ed6aed87c90588a2d15d6d640e51d0
omallech <personrnan@gmail.com>
2026-01-28 16:46:15 +0000

    missed clarification of comment on longitude comment

--- backend/models/population_models.py
+++ backend/models/population_models.py
@@ -2,5 +2,5 @@ class PopulationMetrics:
     def __init__(self, area_name, latitude, longitude, quantity):
         self.area_name = area_name
         self.latitude = latitude  # latitude coordinate of the center of the area
-        self.longitude = longitude  # longitude coordinate of the area
+        self.longitude = longitude  # longitude coordinate of center of the area
         self.quantity = quantity  # census population in this area
