b073f9861de94d9f30ed97fe63043df9e46b5f0f
omallech <personrnan@gmail.com>
2026-01-28 16:41:26 +0000

    changed comments in population_models.py to be more descriptive of latitude and longitude

--- backend/models/population_models.py
+++ backend/models/population_models.py
@@ -1,6 +1,6 @@
 class PopulationMetrics:
     def __init__(self, area_name, latitude, longitude, quantity):
         self.area_name = area_name
-        self.latitude = latitude  # latitude coordinate of the area
+        self.latitude = latitude  # latitude coordinate of the center of the area
         self.longitude = longitude  # longitude coordinate of the area
         self.quantity = quantity  # census population in this area
