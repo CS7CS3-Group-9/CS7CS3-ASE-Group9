8b24d2f28eeef1521a853e8dd1abacb0fcd44cb8
omallech <personrnan@gmail.com>
2026-02-03 15:44:11 +0000

    Removed price information from model and adapter as api doesnt return price information. Created analytics and analytics tests for tourist attraction data

--- backend/adapters/adapters_tests/conftest.py
+++ backend/adapters/adapters_tests/conftest.py
@@ -2,5 +2,5 @@ import sys
 from pathlib import Path
 
 # Add project root to Python path
-project_root = Path(__file__).parent.parent.parent
+project_root = Path(__file__).resolve().parents[3]
 sys.path.insert(0, str(project_root))
